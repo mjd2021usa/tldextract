dc4712a43a1beff784d05e891b7ac6772fd9d3a7
Michael J. Donahue <mdonahue@godaddy.com>
2021-08-10 18:36:08 -0500

    re-add

---
+++ support.go
@@ -0,0 +1,201 @@
+package tldextract
+
+import (
+	"fmt"
+	"io/fs"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func SafeParseInt(str string, base int, bitSize int, defaultValue int64) int64 {
+	result, err := strconv.ParseInt(str, base, bitSize)
+	if err != nil {
+		return defaultValue
+	}
+	return result
+}
+
+func GetEnvString(envVar string, defaultValue string) string {
+	val := os.Getenv(envVar)
+	val = strings.TrimSpace(val)
+	if len(val) <= 0 {
+		val = defaultValue
+	}
+	return val
+}
+
+func GetEnvInt64(envVar string, base int, bitSize int, defaultValue int64) int64 {
+	val := os.Getenv(envVar)
+	val = strings.TrimSpace(val)
+	result, err := strconv.ParseInt(val, base, bitSize)
+	if err != nil {
+		return defaultValue
+	}
+	return result
+}
+
+// SubDomain - return sub-domain, domain
+func SubDomain(domain string) (string, string) {
+	splits := strings.Split(domain, ".")
+	cnt := len(splits)
+	if cnt == 1 {
+		return "", domain
+	}
+	return strings.Join(splits[0:cnt-1], "."), splits[cnt-1]
+}
+
+func IsIPv4(ip net.IP) bool {
+	return ip.To4() != nil
+}
+
+func IsIPv6(ip net.IP) bool {
+	return ip.To16() != nil
+}
+
+// CreateList -
+func CreateList(timeout int64) map[string]struct{} {
+	urls := []string{
+		"https://publicsuffix.org/list/public_suffix_list.dat",                              // Main list
+		"https://raw.githubusercontent.com/publicsuffix/list/master/public_suffix_list.dat", // Fallback
+	}
+	uniqueList := make(map[string]struct{})
+	for _, url := range urls {
+		data, err := DownloadFile(url, timeout)
+		if err != nil {
+			continue
+		}
+		resp := NormalizeLines(string(data))
+		for _, item := range resp {
+			uniqueList[item] = struct{}{}
+		}
+	}
+	return uniqueList
+}
+
+// CreateNewCacheFile - create new cache file from URLs
+func CreateNewCacheFile(fqdn string, urls []string, timeout int64) (map[string]struct{}, error) {
+	uniqueList := DownloadUrls2List(urls, timeout)
+	keys := GetKeys(uniqueList)
+	if len(keys) > 0 {
+		buf := strings.Join(keys, "\n")
+		err := WriteFile(fqdn, []byte(buf))
+		return uniqueList, err
+	}
+	return nil, fmt.Errorf("no records found - skipping overwrite")
+}
+
+// LoadCache - Load cache file with Refresh and fail over options
+func LoadCache(fqdn string, urls []string, refresh bool, timeout int64) (map[string]struct{}, error) {
+	if refresh {
+		// Refresh cache from URLs
+		uList, err := CreateNewCacheFile(fqdn, urls, timeout)
+		if err != nil || len(uList) <= 0 {
+			// Fall back to cache file
+			return LoadCacheFile(fqdn)
+		}
+		return uList, err
+	}
+	uList, err := LoadCacheFile(fqdn)
+	if err != nil || len(uList) <= 0 {
+		// try to refresh cache from URLs
+		return CreateNewCacheFile(fqdn, urls, timeout)
+	}
+	return uList, err
+}
+
+// GetKeys - get list of keys from generic map as string array
+func GetKeys(list map[string]struct{}) []string {
+	keys := make([]string, 0, len(list))
+	for key := range list {
+		keys = append(keys, key)
+	}
+	return keys
+}
+
+// LoadCacheFile - read cache file and Normalize contents (remove comments, split lines, etc)
+func LoadCacheFile(fqdn string) (map[string]struct{}, error) {
+	data, err := ReadFile(fqdn)
+	if err != nil {
+		return nil, err
+	}
+	// Normalize buffer into string array
+	resp := NormalizeLines(string(data))
+	// Add lines to unique list
+	uList := make(map[string]struct{})
+	for _, item := range resp {
+		uList[item] = struct{}{}
+	}
+	return uList, nil
+}
+
+// DownloadUrls2List - Download N number of URLs and merge the unique rows into a generic key map
+func DownloadUrls2List(urls []string, timeout int64) map[string]struct{} {
+	uList := make(map[string]struct{})
+	for _, url := range urls {
+		data, err := DownloadFile(url, timeout)
+		if err != nil {
+			// nothing good, move to next file
+			continue
+		}
+		// Normalize buffer into string array
+		resp := NormalizeLines(string(data))
+		// Add lines to unique list
+		for _, item := range resp {
+			uList[item] = struct{}{}
+		}
+	}
+	return uList
+}
+
+// RemoveNoiseLines - remove blank lines and comments, converting each kept line to lowercase
+func RemoveNoiseLines(srcLines []string) []string {
+	dstLines := []string{}
+	for _, line := range srcLines {
+		line = strings.TrimSpace(line)
+		if line != "" && !strings.HasPrefix(line, "//") {
+			dstLines = append(dstLines, strings.ToLower(line))
+		}
+	}
+	return dstLines
+}
+
+// NormalizeLines - split string into array of strings and remove noise lines
+func NormalizeLines(buffer string) []string {
+	lines := strings.Split(buffer, "\n")
+	return RemoveNoiseLines(lines)
+}
+
+// ReadFile - Read file into byte array
+func ReadFile(fqfn string) ([]byte, error) {
+	return ioutil.ReadFile(fqfn)
+}
+
+// WriteFile - Write byte array into file
+func WriteFile(fqfn string, buffer []byte) error {
+	return ioutil.WriteFile(fqfn, buffer, fs.FileMode(0644))
+}
+
+// DownloadFile - Get body from URL
+func DownloadFile(url string, timeout int64) ([]byte, error) {
+	client := http.Client{
+		Timeout: time.Duration(timeout) * time.Second,
+	}
+
+	resp, err := client.Get(url)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode == 200 {
+		return ioutil.ReadAll(resp.Body)
+	}
+
+	return []byte{}, fmt.Errorf("HTTP Status Code: %d returned", resp.StatusCode)
+}
---
+++ tldextract_test.go
@@ -0,0 +1,266 @@
+package tldextract
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_New_empty_file(t *testing.T) {
+	assert := assert.New(t)
+
+	actual, err := New("", false)
+
+	assert.Nil(actual, "Result should be nil")
+	assert.NotNil(err, "Error should not be nil")
+}
+
+func Test_New_missing_cache_file(t *testing.T) {
+	assert := assert.New(t)
+
+	actual, err := New("i.do.not.exist.cache", false)
+
+	assert.Nil(err, "Error nil")
+	assert.NotNil(actual, "Result not nil")
+}
+
+func Test_New_good_cache_file(t *testing.T) {
+	assert := assert.New(t)
+
+	actual, err := New("test/tld.cache", false)
+
+	assert.Nil(err, "Error nil")
+	assert.NotNil(actual, "Result not nil")
+}
+
+func assertResult(t *testing.T, url string, expected *Result, actual *Result, description string) {
+	if (expected.Flag == actual.Flag) &&
+		(expected.SubDomain == actual.SubDomain) &&
+		(expected.Domain == actual.Domain) &&
+		(expected.Tld == actual.Tld) {
+		return
+	}
+	t.Errorf("%s - %s - expected:%+v, actual:%+v", description, url, expected, actual)
+}
+
+func Test_Extract(t *testing.T) {
+	assert := assert.New(t)
+
+	tld, err := New("test/tld.cache", false)
+	assert.Nil(err, "Error nil")
+
+	testCases := []struct {
+		Url            string
+		ExpectedResult Result
+		ExpectedError  error
+		Description    string
+	}{
+		{
+			Url:            "",
+			ExpectedResult: Result{Flag: Malformed, SubDomain: "", Domain: "", Tld: ""},
+			ExpectedError:  nil,
+			Description:    "empty string",
+		},
+		{
+			Url:            "users@myhost.com",
+			ExpectedResult: Result{Flag: Domain, SubDomain: "", Domain: "myhost", Tld: "com"},
+			ExpectedError:  nil,
+			Description:    "user@ address",
+		},
+		{
+			Url:            "mailto:users@myhost.com",
+			ExpectedResult: Result{Flag: Domain, SubDomain: "", Domain: "myhost", Tld: "com"},
+			ExpectedError:  nil,
+			Description:    "email address",
+		},
+		{
+			Url:            "myhost.com:999",
+			ExpectedResult: Result{Flag: Domain, SubDomain: "", Domain: "myhost", Tld: "com"},
+			ExpectedError:  nil,
+			Description:    "host:port",
+		},
+		{
+			Url:            "myhost.com",
+			ExpectedResult: Result{Flag: Domain, SubDomain: "", Domain: "myhost", Tld: "com"},
+			ExpectedError:  nil,
+			Description:    "basic host",
+		},
+		{
+			Url:            "255.255.myhost.com",
+			ExpectedResult: Result{Flag: Domain, SubDomain: "255.255", Domain: "myhost", Tld: "com"},
+			ExpectedError:  nil,
+			Description:    "basic host with numerit subdomains",
+		},
+		{
+			Url:            "https://user:pass@foo.myhost.com:999/some/path?param1=value1&param2=value2",
+			ExpectedResult: Result{Flag: Domain, SubDomain: "foo", Domain: "myhost", Tld: "com"},
+			ExpectedError:  nil,
+			Description:    "Full URL with subdomain",
+		},
+		{
+			Url:            "http://www.duckduckgo.com",
+			ExpectedResult: Result{Flag: Domain, SubDomain: "www", Domain: "duckduckgo", Tld: "com"},
+			ExpectedError:  nil,
+			Description:    "Full URL with subdomain",
+		},
+		{
+			Url:            "http://duckduckgo.co.uk/path?param1=value1&param2=value2&param3=value3&param4=value4&src=https%3A%2F%2Fwww.yahoo.com%2F",
+			ExpectedResult: Result{Flag: Domain, SubDomain: "", Domain: "duckduckgo", Tld: "co.uk"},
+			ExpectedError:  nil,
+			Description:    "Full HTTP URL with no subdomain",
+		},
+		{
+			Url:            "http://big.long.sub.domain.duckduckgo.co.uk/path?param1=value1&param2=value2&param3=value3&param4=value4&src=https%3A%2F%2Fwww.yahoo.com%2F",
+			ExpectedResult: Result{Flag: Domain, SubDomain: "big.long.sub.domain", Domain: "duckduckgo", Tld: "co.uk"},
+			ExpectedError:  nil,
+			Description:    "Full HTTP URL with subdomain",
+		},
+		{
+			Url:            "https://duckduckgo.co.uk/path?param1=value1&param2=value2&param3=value3&param4=value4&src=https%3A%2F%2Fwww.yahoo.com%2F",
+			ExpectedResult: Result{Flag: Domain, SubDomain: "", Domain: "duckduckgo", Tld: "co.uk"},
+			ExpectedError:  nil,
+			Description:    "Full HTTPS URL with no subdomain",
+		},
+		{
+			Url:            "ftp://peterparker:multipass@mail.duckduckgo.co.uk:666/path?param1=value1&param2=value2&param3=value3&param4=value4&src=https%3A%2F%2Fwww.yahoo.com%2F",
+			ExpectedResult: Result{Flag: Domain, SubDomain: "mail", Domain: "duckduckgo", Tld: "co.uk"},
+			ExpectedError:  nil,
+			Description:    "Full ftp URL with subdomain",
+		},
+		{
+			Url:            "git+ssh://www.github.com/",
+			ExpectedResult: Result{Flag: Domain, SubDomain: "www", Domain: "github", Tld: "com"},
+			ExpectedError:  nil,
+			Description:    "Full git+ssh URL with subdomain",
+		},
+		{
+			Url:            "git+ssh://www.!github.com/",
+			ExpectedResult: Result{Flag: Malformed, SubDomain: "", Domain: "", Tld: ""},
+			ExpectedError:  nil,
+			Description:    "Full git+ssh URL with bad domain",
+		},
+		{
+			Url:            "ssh://server.domain.com/",
+			ExpectedResult: Result{Flag: Domain, SubDomain: "server", Domain: "domain", Tld: "com"},
+			ExpectedError:  nil,
+			Description:    "Full ssh URL with subdomain",
+		},
+		{
+			Url:            "//server.domain.com/path",
+			ExpectedResult: Result{Flag: Domain, SubDomain: "server", Domain: "domain", Tld: "com"},
+			ExpectedError:  nil,
+			Description:    "Missing protocol URL with subdomain",
+		},
+		{
+			Url:            "server.domain.com/path",
+			ExpectedResult: Result{Flag: Domain, SubDomain: "server", Domain: "domain", Tld: "com"},
+			ExpectedError:  nil,
+			Description:    "Full ssh URL with subdomain",
+		},
+		{
+			Url:            "10.10.10.10",
+			ExpectedResult: Result{Flag: IPv4, SubDomain: "", Domain: "10.10.10.10", Tld: ""},
+			ExpectedError:  nil,
+			Description:    "Basic IPv4 Address",
+		},
+		{
+			Url:            "http://10.10.10.10",
+			ExpectedResult: Result{Flag: IPv4, SubDomain: "", Domain: "10.10.10.10", Tld: ""},
+			ExpectedError:  nil,
+			Description:    "Basic IPv4 Address URL",
+		},
+		{
+			Url:            "http://10.10.10.256",
+			ExpectedResult: Result{Flag: Malformed, SubDomain: "", Domain: "", Tld: ""},
+			ExpectedError:  nil,
+			Description:    "Basic IPv4 Address URL with bad IP",
+		},
+		/*
+			{
+				Url:            "http://2001:0db8:0000:0000:0000:ff00:0042:8329",
+				ExpectedResult: Result{Flag: IPv6, SubDomain: "", Domain: "2001:0db8:0000:0000:0000:ff00:0042:8329", Tld: ""},
+				ExpectedError:  nil,
+				Description:    "Basic IPv6 Address URL",
+			},
+			{
+				Url:            "http://2001:db8:0:0:0:ff00:42:8329",
+				ExpectedResult: Result{Flag: IPv6, SubDomain: "", Domain: "2001:db8:0:0:0:ff00:42:8329", Tld: ""},
+				ExpectedError:  nil,
+				Description:    "Basic IPv6 Address URL",
+			},
+			{
+				Url:            "http://2001:db8::ff00:42:8329",
+				ExpectedResult: Result{Flag: IPv6, SubDomain: "", Domain: "2001:db8::ff00:42:8329", Tld: ""},
+				ExpectedError:  nil,
+				Description:    "Basic IPv6 Address URL",
+			},
+			{
+				Url:            "http://::ffff:192.0.2.128",
+				ExpectedResult: Result{Flag: IPv6, SubDomain: "", Domain: "::ffff:192.0.2.128", Tld: ""},
+				ExpectedError:  nil,
+				Description:    "Basic IPv6 Address URL",
+			},
+			{
+				Url:            "http://::192.0.2.128",
+				ExpectedResult: Result{Flag: IPv6, SubDomain: "", Domain: "::192.0.2.128", Tld: ""},
+				ExpectedError:  nil,
+				Description:    "Basic IPv6 Address URL",
+			},
+		*/
+		{
+			Url:            "http://godaddy.godaddy",
+			ExpectedResult: Result{Flag: Domain, SubDomain: "", Domain: "godaddy", Tld: "godaddy"},
+			ExpectedError:  nil,
+			Description:    "Basic URL",
+		},
+		{
+			Url:            "http://godaddy.godaddy.godaddy",
+			ExpectedResult: Result{Flag: Domain, SubDomain: "godaddy", Domain: "godaddy", Tld: "godaddy"},
+			ExpectedError:  nil,
+			Description:    "Basic URL with subdomain",
+		},
+		{
+			Url:            "http://godaddy.godaddy.co.uk",
+			ExpectedResult: Result{Flag: Domain, SubDomain: "godaddy", Domain: "godaddy", Tld: "co.uk"},
+			ExpectedError:  nil,
+			Description:    "Basic URL with subdomain",
+		},
+		{
+			Url:            "http://godaddy",
+			ExpectedResult: Result{Flag: Malformed, SubDomain: "", Domain: "", Tld: ""},
+			ExpectedError:  nil,
+			Description:    "Basic URL with TLD only",
+		},
+		{
+			Url:            "http://godaddy.cannon-fodder",
+			ExpectedResult: Result{Flag: Malformed, SubDomain: "", Domain: "", Tld: ""},
+			ExpectedError:  nil,
+			Description:    "Basic URL with bad TLD",
+		},
+		{
+			Url:            "http://godaddy.godaddy.cannon-fodder",
+			ExpectedResult: Result{Flag: Malformed, SubDomain: "", Domain: "", Tld: ""},
+			ExpectedError:  nil,
+			Description:    "Basic URL with subdomainand bad TLD",
+		},
+		{
+			Url:            "http://domainer.个人.hk",
+			ExpectedResult: Result{Flag: Domain, SubDomain: "", Domain: "domainer", Tld: "个人.hk"},
+			ExpectedError:  nil,
+			Description:    "Basic URL with partial encoded punycode TLD",
+		},
+		{
+			Url:            "http://domainer.公司.香港",
+			ExpectedResult: Result{Flag: Domain, SubDomain: "", Domain: "domainer", Tld: "公司.香港"},
+			ExpectedError:  nil,
+			Description:    "Basic URL with fully encoded punycode TLD",
+		},
+	}
+
+	for _, tc := range testCases {
+		actualResult := tld.Extract(tc.Url)
+
+		//assert.Equal(tc.ExpectedError, actualErr, "Error are equal")
+		assertResult(t, tc.Url, &tc.ExpectedResult, actualResult, tc.Description)
+	}
+}
